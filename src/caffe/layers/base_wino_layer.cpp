532753ca510904182038baa273a0905fb0ecfb79
Swastik <swas.rishi@gmail.com>
2019-07-10 19:46:50 +0100

    basic

--- src/caffe/layers/base_wino_layer.cpp
+++ src/caffe/layers/base_wino_layer.cpp
@@ -485,7 +485,7 @@ namespace caffe {
         const int output_w = (input_w + 2 * pad_w - (dilation_w * (kernel_w - 1) + 1)) / stride_w + 1;
         const int channel_size = input_h * input_w;
         const int out_channel_size = output_h*output_w;
-        cudaMemset(output,0, output_w);
+        cudaMemset(output,0, sizeof(Dtype)*output_h*1*out_channels);
 
         // // parameters of padding and tiling
         // int tile_num_w = (input_w + 2 * pad_w-6) / 4 + ((input_w + 2 * pad_w-6) % 4 > 0 ? 1 : 0)+1;
