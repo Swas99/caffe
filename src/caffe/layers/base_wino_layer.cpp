8b6f6d417486e555f2628f30787401b541239869
Swastik <swas.rishi@gmail.com>
2019-07-10 19:28:59 +0100

    basic

--- src/caffe/layers/base_wino_layer.cpp
+++ src/caffe/layers/base_wino_layer.cpp
@@ -464,14 +464,14 @@ namespace caffe {
         if (kernel_h != 3 || kernel_w != 3) {
             LOG(FATAL) << "kernel size must be 3";
         }
-        if (pad_h>4||pad_w>4)
-        {
-            LOG(FATAL) << "padding must less than 4";
-        }
+        // if (pad_h>4||pad_w>4)
+        // {
+        //     LOG(FATAL) << "padding must less than 4";
+        // }
 
-        if (group_ > 1) {
-            LOG(FATAL) << "multi Groups not implemented ";
-        }
+        // if (group_ > 1) {
+        //     LOG(FATAL) << "multi Groups not implemented ";
+        // }
 
         // Dtype weight[3][3];  //kernel weight
         // Dtype in[6][6]; //input tile
