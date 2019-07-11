96432b68be1f25f4540fe1043e173ca78da233c8
Swastik <swas.rishi@gmail.com>
2019-07-11 15:59:01 +0100

    basic

--- src/caffe/layers/base_wino_layer.cpp
+++ src/caffe/layers/base_wino_layer.cpp
@@ -577,6 +577,14 @@ namespace caffe {
         cudaMalloc(&padded_input, in_channels*padded_channel_size* sizeof(Dtype));
         cudaMemset(padded_input,0, sizeof(Dtype)*in_channels*padded_channel_size);
 
+        for (int c=0;c<in_channels;c++)
+            for (int h=0;h<input_h;h++)
+                for (int w=0;w<input_w;w++)
+                {
+                     padded_input[c*padded_channel_size+padded_in_w*(h+pad_h)+w+pad_w];
+                     input[c*channel_size+h*input_w+w];
+                }
+
         //copy input to padded_input
         // for (int c=0;c<in_channels;c++)
         //     for (int h=0;h<input_h;h++)
