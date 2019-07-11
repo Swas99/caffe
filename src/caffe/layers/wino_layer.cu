40f60da32419238a76e8f96e3e6688c0fde1cac1
Swastik <swas.rishi@gmail.com>
2019-07-11 14:06:03 +0100

    basic

--- src/caffe/layers/wino_layer.cu
+++ src/caffe/layers/wino_layer.cu
@@ -4,53 +4,6 @@
 
 namespace caffe {
 
-    template<typename Dtype>
-    void WinogradLayer<Dtype>::compute_output_shape() {
-        const int *kernel_shape_data = this->kernel_shape_.gpu_data();
-        const int *stride_data = this->stride_.gpu_data();
-        const int *pad_data = this->pad_.gpu_data();
-        const int *dilation_data = this->dilation_.gpu_data();
-        this->output_shape_.clear();
-        for (int i = 0; i < this->num_spatial_axes_; ++i) {
-            // i + 1 to skip channel axis
-            const int input_dim = this->input_shape(i + 1);
-            const int kernel_extent = dilation_data[i] * (kernel_shape_data[i] - 1) + 1;
-            const int output_dim = (input_dim + 2 * pad_data[i] - kernel_extent)
-                                   / stride_data[i] + 1;
-            this->output_shape_.push_back(output_dim);
-        }
-    }
-
-    template<typename Dtype>
-    void WinogradLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype> *> &bottom,
-                                              const vector<Blob<Dtype> *> &top) {
-        const Dtype *weight = this->blobs_[0]->gpu_data();
-        for (int i = 0; i < bottom.size(); ++i) {
-            const Dtype *bottom_data = bottom[i]->gpu_data();
-            Dtype *top_data = top[i]->mutable_gpu_data();
-
-
-            //xxx(bottom_data, weight, top_data, this->num_);
-
-            for (int n = 0; n < this->num_; ++n) {
-                const int *kernel_shape_data = this->kernel_shape_.gpu_data();
-                if (kernel_shape_data[i] < 3) //kernel size !=3 has not implemented
-                    this->forward_gpu_gemm(bottom_data + n * this->bottom_dim_, weight,
-                                           top_data + n * this->top_dim_);
-                else {
-                    //this->forward_gpu_winograd(bottom_data + n * this->bottom_dim_, weight,
-                    //                           top_data + n * this->top_dim_);
-                    this->forward_gpu_gemm(bottom_data + n * this->bottom_dim_, weight,
-                                           top_data + n * this->top_dim_);
-                }
-
-                if (this->bias_term_) {
-                    const Dtype *bias = this->blobs_[1]->gpu_data();
-                    this->forward_gpu_bias(top_data + n * this->top_dim_, bias);
-                }
-            }
-        }
-    }
 
 
 
@@ -230,6 +183,54 @@ namespace caffe {
         Winograd2x2ImTransCompute<float><<<gridDim, blockDim>>>(Input, TransIm, C, B, H, W, pad_h, pad_w);
     }
 
+
+    template<typename Dtype>
+    void WinogradLayer<Dtype>::compute_output_shape() {
+        const int *kernel_shape_data = this->kernel_shape_.gpu_data();
+        const int *stride_data = this->stride_.gpu_data();
+        const int *pad_data = this->pad_.gpu_data();
+        const int *dilation_data = this->dilation_.gpu_data();
+        this->output_shape_.clear();
+        for (int i = 0; i < this->num_spatial_axes_; ++i) {
+            // i + 1 to skip channel axis
+            const int input_dim = this->input_shape(i + 1);
+            const int kernel_extent = dilation_data[i] * (kernel_shape_data[i] - 1) + 1;
+            const int output_dim = (input_dim + 2 * pad_data[i] - kernel_extent)
+                                   / stride_data[i] + 1;
+            this->output_shape_.push_back(output_dim);
+        }
+    }
+
+    template<typename Dtype>
+    void WinogradLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype> *> &bottom,
+                                              const vector<Blob<Dtype> *> &top) {
+        const Dtype *weight = this->blobs_[0]->gpu_data();
+        for (int i = 0; i < bottom.size(); ++i) {
+            const Dtype *bottom_data = bottom[i]->gpu_data();
+            Dtype *top_data = top[i]->mutable_gpu_data();
+
+
+            xxx(bottom_data, weight, top_data, this->num_);
+
+            for (int n = 0; n < this->num_; ++n) {
+                const int *kernel_shape_data = this->kernel_shape_.gpu_data();
+                if (kernel_shape_data[i] < 3) //kernel size !=3 has not implemented
+                    this->forward_gpu_gemm(bottom_data + n * this->bottom_dim_, weight,
+                                           top_data + n * this->top_dim_);
+                else {
+                    //this->forward_gpu_winograd(bottom_data + n * this->bottom_dim_, weight,
+                    //                           top_data + n * this->top_dim_);
+                    this->forward_gpu_gemm(bottom_data + n * this->bottom_dim_, weight,
+                                           top_data + n * this->top_dim_);
+                }
+
+                if (this->bias_term_) {
+                    const Dtype *bias = this->blobs_[1]->gpu_data();
+                    this->forward_gpu_bias(top_data + n * this->top_dim_, bias);
+                }
+            }
+        }
+    }
 
     template<typename Dtype>
     void WinogradLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype> *> &top,
