d89c4d03cf40eea1842c0aba6d45b5082cba3cce
Swastik <swas.rishi@gmail.com>
2019-07-11 10:26:07 +0100

    basic

--- src/caffe/layers/base_wino_layer.cpp
+++ src/caffe/layers/base_wino_layer.cpp
@@ -533,12 +533,13 @@ namespace caffe {
         int dilation_w   = dilation_.gpu_data()[1];
         int kernel_size  = kernel_h * kernel_w;
 
+        printf("%d : %d\n",kernel_h, kernel_w);
         if (kernel_h != 3 || kernel_w != 3) {
             // LOG(FATAL) << "kernel size must be 3";
         }
         else
         {
-            // printf("%d : %d\n",kernel_h, kernel_w);
+            
         }
         if (pad_h>4||pad_w>4)
         {
