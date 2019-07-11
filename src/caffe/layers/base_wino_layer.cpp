42376b58363c3070c5871e1c082fd78fc7e1b185
Swastik <swas.rishi@gmail.com>
2019-07-11 18:34:30 +0100

    cuda

--- src/caffe/layers/base_wino_layer.cpp
+++ src/caffe/layers/base_wino_layer.cpp
@@ -8,9 +8,9 @@
 #include "caffe/util/math_functions.hpp"
 
 
-// #include "tensorflow/core/framework/op.h"
-// #include "tensorflow/core/framework/op_kernel.h"
-// #include "tensorflow/core/framework/shape_inference.h"
+#include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/op_kernel.h"
+#include "tensorflow/core/framework/shape_inference.h"
 
 
 namespace caffe {
@@ -608,21 +608,21 @@ namespace caffe {
         int padded_out_channel_size = padded_out_w*padded_out_h;
 
         // Dtype*padded_out = (Dtype*)malloc(padded_out_channel_size*out_channels* sizeof(Dtype));
-        Dtype*padded_out;
-        cudaMalloc((void **)&padded_out, padded_out_channel_size*out_channels* sizeof(Dtype));
-        cudaMemset(padded_out,0, sizeof(Dtype)*padded_out_channel_size*out_channels);
+        float*padded_out;
+        cudaMalloc((void **)&padded_out, padded_out_channel_size*out_channels* sizeof(float));
+        cudaMemset(padded_out,0, sizeof(float)*padded_out_channel_size*out_channels);
 
         //pad 0
-        // Dtype* padded_input = (Dtype*)malloc(in_channels*padded_channel_size* sizeof(Dtype));
-        Dtype*padded_input;
-        cudaMalloc((void **)&padded_input, in_channels*padded_channel_size* sizeof(Dtype));
-        cudaMemset(padded_input,0, sizeof(Dtype)*in_channels*padded_channel_size);
+        // float* padded_input = (float*)malloc(in_channels*padded_channel_size* sizeof(float));
+        float*padded_input;
+        cudaMalloc((void **)&padded_input, in_channels*padded_channel_size* sizeof(float));
+        cudaMemset(padded_input,0, sizeof(float)*in_channels*padded_channel_size);
 
         for (int c=0;c<in_channels;c++)
             for (int h=0;h<input_h;h++)
                 for (int w=0;w<input_w;w++)
                 {
-                     padded_input[c*padded_channel_size+padded_in_w*(h+pad_h)+w+pad_w];
+                     padded_input[c*padded_channel_size+padded_in_w*(h+pad_h)+w+pad_w]=
                      input[c*channel_size+h*input_w+w];
                 }
 
