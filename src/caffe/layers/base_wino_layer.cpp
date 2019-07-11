4ad8090b41c54c936669b44d92566dd71252b89a
Swastik <swas.rishi@gmail.com>
2019-07-11 18:41:28 +0100

    cuda

--- src/caffe/layers/base_wino_layer.cpp
+++ src/caffe/layers/base_wino_layer.cpp
@@ -631,8 +631,8 @@ namespace caffe {
             for (int h=0;h<input_h;h++)
                 for (int w=0;w<input_w;w++)
                 {
-                     *padded_input + c*padded_channel_size+padded_in_w*(h+pad_h)+w+pad_w];
-                     *input + c*channel_size+h*input_w+w];
+                     *padded_input + c*padded_channel_size+padded_in_w*(h+pad_h)+w+pad_w;
+                     *input + c*channel_size+h*input_w+w;
                 }
 
         //copy input to padded_input
