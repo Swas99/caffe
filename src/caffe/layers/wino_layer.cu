3a57e237b6cd6099d00ba66877275fa17395f268
Swastik <swas.rishi@gmail.com>
2019-07-14 17:08:07 +0100

    cuda

--- src/caffe/layers/wino_layer.cu
+++ src/caffe/layers/wino_layer.cu
@@ -370,8 +370,8 @@ void Winograd2x2ConvComputeLauncher(const float *Input, const float *Weight, flo
                                                const vector<bool> &propagate_down,
                                                const vector<Blob<Dtype> *> &bottom) {
 
-        const int *kernel_shape_data = this->kernel_shape_.gpu_data();
-        printf("K: %d \n", kernel_shape_data[8]);
+        const int *kernel_shape_data = this->kernel_shape_.cpu_data();
+        printf("K: %d \n", kernel_shape_data[0]);
 
 
         const Dtype *weight = this->blobs_[0]->gpu_data();
