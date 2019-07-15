eb6c4b1c112f97bc5e15efc909159eebd063f4ee
Swastik <swas.rishi@gmail.com>
2019-07-15 16:40:42 +0100

    ..

--- src/caffe/layers/wino_layer.cpp
+++ src/caffe/layers/wino_layer.cpp
@@ -34,8 +34,10 @@ namespace caffe {
                     this->forward_cpu_gemm(bottom_data + n * this->bottom_dim_, weight,
                                            top_data + n * this->top_dim_);
                 else {
-                    this->forward_cpu_winograd(bottom_data + n * this->bottom_dim_, weight,
-                                               top_data + n * this->top_dim_);
+                    // this->forward_cpu_winograd(bottom_data + n * this->bottom_dim_, weight,
+                    //                            top_data + n * this->top_dim_);
+                    this->forward_cpu_gemm(bottom_data + n * this->bottom_dim_, weight,
+                                           top_data + n * this->top_dim_);
                 }
 
                 if (this->bias_term_) {
