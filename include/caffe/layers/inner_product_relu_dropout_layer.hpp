d62b8a82f17c9517fe4a0f2d920bdc4553d59fea
Swastik <swas.rishi@gmail.com>
2019-07-19 05:58:04 +0100

    winograd.hpp

---
+++ include/caffe/layers/inner_product_relu_dropout_layer.hpp
@@ -0,0 +1,67 @@
+#ifndef CAFFE_INNER_PRODUCT_RELU_DROPOUT_LAYER_HPP_
+#define CAFFE_INNER_PRODUCT_RELU_DROPOUT_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+#include "caffe/util/libxsmm_spmv.h"
+#include "caffe/layers/inner_product_layer.hpp"
+
+#include "libxsmm.h"
+
+namespace caffe {
+
+/**
+ * @brief Also known as a "fully-connected" layer, computes an inner product
+ *        with a set of learned weights, and (optionally) adds biases.
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+template <typename Dtype>
+class InnerProductReLUDropoutLayer : public Layer<Dtype> {
+ public:
+  explicit InnerProductReLUDropoutLayer(const LayerParameter& param);
+  ~InnerProductReLUDropoutLayer();
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "InnerProductReLUDropout"; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+  virtual void WeightAlign();
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  int M_; ///< batch size ///
+  int K_; ///< input size ///
+  int N_; ///< output size ///
+  bool bias_term_;
+  Blob<Dtype> bias_multiplier_;
+  bool transpose_;  ///< if true, assume transposed weights
+
+  libxsmm_spmdm_handle libxsmm_spmdm_handle_;
+#ifdef OPTIMIZE_FOR_UNIT_BATCH
+  libxsmm_spmv_handle libxsmm_spmv_handle_; // for batch size 1
+#endif
+
+  libxsmm_CSR_sparseslice *libxsmm_csr_weight_;
+  int nnz_weight_;
+
+  Dtype *bottom_transposed_;
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_INNER_PRODUCT_RELU_DROPOUT_LAYER_HPP_
