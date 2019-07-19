1321875ee94c51bb041606c26b98e59aaf87199e
Swastik <swas.rishi@gmail.com>
2019-07-19 07:36:33 +0100

    winograd.hpp

--- include/caffe/solver.hpp
+++ include/caffe/solver.hpp
@@ -135,7 +135,7 @@ class Solver {
   // True iff a request to stop early was received.
   bool requested_early_exit_;
 
-  Dtype prune_threshold_;
+  static float prune_threshold_;
   Dtype measure_threshold_;
 
   DISABLE_COPY_AND_ASSIGN(Solver);
