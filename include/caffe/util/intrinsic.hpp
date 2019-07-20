c20c3c6e3c3f6b98c47299d32ae82aa1d9b04637
Swastik <swas.rishi@gmail.com>
2019-07-20 11:09:31 +0100

    retry skim

--- include/caffe/util/intrinsic.hpp
+++ include/caffe/util/intrinsic.hpp
@@ -48,7 +48,7 @@
 #define _MM_ADD(a, b) _mm256_add_ps(a, b)
 #define _MM_SUB(a, b) _mm256_sub_ps(a, b)
 #define _MM_MUL(a, b) _mm256_mul_ps(a, b)
-#define _MM_POW(a, b) _mm256_pow_ps(a, b)
+#define _MM_POW(a, b) _mm256_exp2_ps(a, b)
 #define _MM_FMADD(a, b, c) _mm256_fmadd_ps(a, b, c)
 
 #define _MM_MAX(a, b) _mm256_max_ps(a, b)
