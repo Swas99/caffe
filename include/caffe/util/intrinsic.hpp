99defb26b27e35259f92d41a7cc37f8fd383e366
Swastik <swas.rishi@gmail.com>
2019-07-20 10:54:25 +0100

    retry skim

--- include/caffe/util/intrinsic.hpp
+++ include/caffe/util/intrinsic.hpp
@@ -2,6 +2,7 @@
 #define _CAFFE_UTIL_INTRINSIC_HPP_
 
 // #include <immintrin.h>
+#include <cmath>
 
 #ifdef _MSC_VER
 #include <intrin.h>
@@ -49,7 +50,7 @@
 #define _MM_SUB(a, b) _mm256_sub_ps(a, b)
 #define _MM_MUL(a, b) _mm256_mul_ps(a, b)
 #define _MM_FMADD(a, b, c) _mm256_fmadd_ps(a, b, c)
-#define _MM_POW(a, b) cmath::pow(a, b)
+#define _MM_POW(a, b) pow(a, b)
 
 #define _MM_MAX(a, b) _mm256_max_ps(a, b)
 
